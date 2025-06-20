2d8bad050189c331c16876b3aa64f18b9ca566e0
devdorong <dev.dorong@gmail.com>
2025-06-20 16:32:26 +0900

    [docs] 배포 1차

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const closeBt = document.querySelector(".popup_close");
+    const popup = document.querySelector(".popup");
+
+    closeBt.addEventListener("click", function() {
+        
+        popup.style.display = "none"
+    })
+});
+
+// window.addEventListener("DOMContentLoaded", function () {
+//   const closeBt = this.document.querySelector(".popup_close");
+//   const popup = this.document.querySelector(".popup");
+
+//   closeBt.addEventListener("click", function () {
+//     popup.style.display = "none";
+//   });
+// });
